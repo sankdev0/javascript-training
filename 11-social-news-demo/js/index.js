6ebb8dff90db7261939cf69697f970ab2a251b0d
san k <sankdev0@gmail.com>
2021-12-23 12:52:32 +0500

    Add Add a link feature

--- 11-social-news-demo/js/index.js
+++ 11-social-news-demo/js/index.js
@@ -15,6 +15,10 @@ class Link {
     }
 }
 
+// Program state
+const lineSeparator = getLineBreakSequence();
+const links = [];
+
 // Credit: function by T.J. Crowder
 // https://stackoverflow.com/a/6833299
 function getLineBreakSequence() {
@@ -27,33 +31,71 @@ function getLineBreakSequence() {
     return text.indexOf("\r") >= 0 ? "\r\n" : "\n";
 }
 
-const lineSeparator = getLineBreakSequence();
+// Get the string for the main menu.
+function getPromptText() {
+    // Build prompt text
+    const menuItems = [
+        "1: Show links",
+        "2: Add a link",
+        "3: Remove a link",
+        "0: Quit"
+    ];
 
-const links = [];
+    let promptText = "Choose an option";
 
+    menuItems.map(x => promptText = promptText + lineSeparator + x);
+
+    return promptText;
+}
+
+// Add the new link to the links array.
 function addLink(link) {
     links.push(link);
 }
 
+// Return a string representing the links array
 function showLinks(links) {
-    links.map(x => console.log(x.show()));
+    if (links.length == 0) {
+        return "";
+    } else if (links.length == 1) {
+        return links[0].show();
+    } else {
+        return links.reduce((string, x) => string + lineSeparator + x.show(), "");
+    }
 }
 
-const menuItems = [
-    "1: Show links",
-    "2: Add a link",
-    "3: Remove a link",
-    "0: Quit"
-];
-
-let promptText = "Choose an option";
-
-menuItems.map(x => promptText = promptText + lineSeparator + x);
-
-const item = prompt(promptText);
-
-if(item == 2) {
-    addLink(new Link(1, "my first link", "google.com", "San K"));
-    showLinks(links);
+// Take user input to create a new Link
+function inputLinkData() {
+    const id = prompt("Input link id:");
+    const title = prompt("Input link title:");
+    const url = prompt("Input link URL:");
+    const author = prompt("Input link Auhor:");
+    return new Link(id, title, url, author);
 }
+
+// Process user menu choices in the main app loop
+function processMenu(promptText) {
+
+    let wantsToQuit = false;
+
+    do {
+        const item = prompt(promptText);
+
+        switch (item) {
+            case "0":
+                wantsToQuit = true;
+                break;
+            case "2":
+                addLink(inputLinkData());
+                console.log(showLinks(links));
+                break;
+            default:
+                break;
+        }
+    } while (!wantsToQuit);
+}
+
+
+// Run the main loop
+processMenu(getPromptText());
 
